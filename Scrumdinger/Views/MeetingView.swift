63517959279b78504c1e6aaff07bbc224c7611bb
Ram Jondhale <ramjondhale1@gmail.com>
2023-10-25 11:37:33 +0530

    Create scrum timer to keep track of time in daily scrum meeting - Create ScrumTimer class which conforms to `ObservableObject` protocol to keep track on time in daily scrum meeting - Add support to start scrum meeting with meeting length and attendees - Add support to keep track on meeting total time and remaining time - Add support to keep track on specker elapsed time and remaining time using timer and switch to next speaker once current speaker time is completed - Add published properties for active speaker name, seconds elapsed and seconds remaining - Add support to skip speaker, change speaker and reset timer. - Create ScrumTimer as a StateObject in the MeetingView - Integrate MeetingHeaderView into meeting view and setup it's properties using scrum timers published properties - Add support to start scum on meeting view appear and stop scrum on view disappear

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct MeetingView: View {
 
     @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
 
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
             VStack {
+                MeetingHeaderView(
+                    secondsElapsed: scrumTimer.secondsElapsed,
+                    secondsRemaining: scrumTimer.secondsRemaining, 
+                    theme: scrum.theme)
                 Circle()
                     .strokeBorder(lineWidth: 24)
 
@@ -33,6 +38,13 @@ struct MeetingView: View {
         }
         .padding()
         .foregroundStyle(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
         .navigationBarTitleDisplayMode(.inline )
     }
 }
