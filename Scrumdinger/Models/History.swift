4b3fc01b486161afa88fae59d47132fbc7366e75
Ram Jondhale <ramjondhale1@gmail.com>
2023-10-26 09:18:03 +0530

    Add support to keep history of scrum meetings - Add new field history of type array of `History` which store previous meetings date, attendees etc - Add new section in `DetailView` to display history of scrum meetings - Append new history in the history array on `MeetingView` disappear

---
+++ Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by Ram Jondhale on 25/10/23.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    let attendees: [DailyScrum.Attendee]
+
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
